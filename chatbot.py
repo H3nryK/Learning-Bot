45a171cff95a6fd862bc131c9ae9c4e1e325830c
H3nryK <henrykimani89@gmail.com>
2024-07-14 10:56:56 +0300

    Create formatted data file

--- chatbot.py
+++ chatbot.py
@@ -28,3 +28,68 @@ def printLines(file, n=10):
         print(line)
 
 printLines(os.path.join(corpus, "utterances.jsonl"))
+
+# Splits each line of the file to create lines and conversations
+def loadLinesAndConversations(fileName):
+    lines = {}
+    conversations = {}
+    with open(fileName, 'r', encoding='iso-8859-1') as f:
+        for line in f:
+            lineJson = json.loads(line)
+            # Extract fields for line object
+            lineObj = {}
+            lineObj["lineID"] = lineJson["id"]
+            lineObj["characterID"] = lineJson["speaker"]
+            lineObj["text"] = lineJson["text"]
+            lines[lineObj['lineID']] = lineObj
+
+            # Extract fields for conversation object
+            if lineJson["conversation_id"] not in conversations:
+                convObj = {}
+                convObj["conversationID"] = lineJson["conversation_id"]
+                convObj["movieID"] = lineJson["meta"]["movie_id"]
+                convObj["lines"] = [lineObj]
+            else:
+                convObj = conversations[lineJson["conversation_id"]]
+                convObj["lines"].insert(0, lineObj)
+            conversations[convObj["conversationID"]] = convObj
+
+    return lines, conversations
+
+# Extracts pairs of sentences from conversations
+def extractSentencePairs(conversations):
+    qa_pairs = []
+    for conversation in conversations.values():
+        # Iterate over all the lines of the conversation
+        for i in range(len(conversation["lines"]) - 1):  # We ignore the last line (no answer for it)
+            inputLine = conversation["lines"][i]["text"].strip()
+            targetLine = conversation["lines"][i+1]["text"].strip()
+            # Filter wrong samples (if one of the lists is empty)
+            if inputLine and targetLine:
+                qa_pairs.append([inputLine, targetLine])
+    return qa_pairs
+
+# Define path to new file
+datafile = os.path.join(corpus, "formatted_movie_lines.txt")
+
+delimiter = '\t'
+# Unescape the delimiter
+delimiter = str(codecs.decode(delimiter, "unicode_escape"))
+
+# Initialize lines dict and conversations dict
+lines = {}
+conversations = {}
+# Load lines and conversations
+print("\nProcessing corpus into lines and conversations...")
+lines, conversations = loadLinesAndConversations(os.path.join(corpus, "utterances.jsonl"))
+
+# Write new csv file
+print("\nWriting newly formatted file...")
+with open(datafile, 'w', encoding='utf-8') as outputfile:
+    writer = csv.writer(outputfile, delimiter=delimiter, lineterminator='\n')
+    for pair in extractSentencePairs(conversations):
+        writer.writerow(pair)
+
+# Print a sample of lines
+print("\nSample lines from file:")
+printLines(datafile)
