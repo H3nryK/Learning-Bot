21e7bbe920d2c993df6571fd757e6e61894c4a71
H3nryK <henrykimani89@gmail.com>
2024-07-14 11:04:11 +0300

    Run Model

--- chatbot.py
+++ chatbot.py
@@ -601,3 +601,49 @@ def evaluateInput(encoder, decoder, searcher, voc):
 
         except KeyError:
             print("Error: Encountered unknown word.")
+
+# Configure models
+model_name = 'cb_model'
+attn_model = 'dot'
+#``attn_model = 'general'``
+#``attn_model = 'concat'``
+hidden_size = 500
+encoder_n_layers = 2
+decoder_n_layers = 2
+dropout = 0.1
+batch_size = 64
+
+# Set checkpoint to load from; set to None if starting from scratch
+loadFilename = None
+checkpoint_iter = 4000
+
+# Load model if a ``loadFilename`` is provided
+if loadFilename:
+    # If loading on same machine the model was trained on
+    checkpoint = torch.load(loadFilename)
+    # If loading a model trained on GPU to CPU
+    #checkpoint = torch.load(loadFilename, map_location=torch.device('cpu'))
+    encoder_sd = checkpoint['en']
+    decoder_sd = checkpoint['de']
+    encoder_optimizer_sd = checkpoint['en_opt']
+    decoder_optimizer_sd = checkpoint['de_opt']
+    embedding_sd = checkpoint['embedding']
+    voc.__dict__ = checkpoint['voc_dict']
+
+
+print('Building encoder and decoder ...')
+# Initialize word embeddings
+embedding = nn.Embedding(voc.num_words, hidden_size)
+if loadFilename:
+    embedding.load_state_dict(embedding_sd)
+# Initialize encoder & decoder models
+encoder = EncoderRNN(hidden_size, embedding, encoder_n_layers, dropout)
+decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, decoder_n_layers, dropout)
+if loadFilename:
+    encoder.load_state_dict(encoder_sd)
+    decoder.load_state_dict(decoder_sd)
+# Use appropriate device
+encoder = encoder.to(device)
+decoder = decoder.to(device)
+print('Models built and ready to go!')
+
